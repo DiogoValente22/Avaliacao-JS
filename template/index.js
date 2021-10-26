0f6cb2d4dc303ef90329aa2c8d7fb2202608581a
DiogoValente22 <diogovalentearagao@outlook.com>
2021-10-26 20:08:26 -0300

    atividade finalizada

--- template/index.js
+++ template/index.js
@@ -15,29 +15,55 @@ class Bill {
     this.items.push(item);
   }
 
-  removeItem = nome => {
-    //TODO
+  removeItem = (nome) => {
+    this.items.splice(nome, 1);
+    bill.render();
+    
+    var elementTotal = document.getElementById('total');
+
+    var total = bill.items.reduce(getTotal, 0);
+    function getTotal(total, item){
+      return total + parseFloat(item.price);
+    }
+    elementTotal.innerHTML = 'R$ ' + '<span id="precoTotal">' + total + '</span>';
+
   }
 
   billTotal = () => {
-    //TODO
+    
+    var elementTotal = document.getElementById('total');
+
+    var total = bill.items.reduce(getTotal, 0);
+    function getTotal(total, item){
+      return total + parseFloat(item.price);
+    }
+    elementTotal.innerHTML = 'R$ ' + '<span id="precoTotal">' + total + '</span>';
   }
 
   render = () => {
-    //UNFINISHED
+    
     let billContainer = document.getElementById('items');
     billContainer.innerHTML = '';
-    this.items.map(item => {
-      let row = document.createElement('tr');
-      let foodName = document.createElement('td');
-      let foodPrice = document.createElement('td');
-      foodName.innerHTML = item.name;
-      foodPrice.innerHTML = 'R$ ' + item.price;
 
-      row.append(foodName);
-      row.append(foodPrice);
-      billContainer.append(row);
-    })
+    if(bill.items.length == 0){
+      var elements = document.querySelector('#items');
+      elements.innerHTML = '<h1 style="margin-left: 100px;">A comanda está vazia</h1>';
+    }else{
+        this.items.map(item => {
+        let posicao = this.items.indexOf(item);
+        let row = document.createElement('tr');
+        let foodName = document.createElement('td');
+        let foodPrice = document.createElement('td');
+        foodName.onclick = function(){
+          bill.removeItem(posicao);
+        }
+        foodName.innerHTML = item.name;
+        foodPrice.innerHTML = 'R$ ' + item.price;
+        row.append(foodName);
+        row.append(foodPrice);
+        billContainer.append(row);
+      })
+    }
   }
 }
 
@@ -45,12 +71,71 @@ class Bill {
 var bill = new Bill();
 
 function init() {
-  bill.addItem(new Item('croissant', 5.99));
-  bill.addItem(new Item('café', 2.99));
-  bill.render();
+
+  // inicializa o valor total para 0 reais
+  let elementTotal = document.getElementById('total');
+  elementTotal.innerHTML = 'R$ 0,00';
+  
+  // Exibe que a comanda está vazia
+  if(bill.items.length == 0){
+    var elements = document.querySelector('#items');
+    elements.innerHTML = '<h1 style="margin-left: 100px;">A comanda está vazia</h1>';
+  }
+
   document.getElementsByTagName('body')[0].style.display = 'flex';
+
+}
+
+function adicionarItem (){
+  
+  var itemName = document.querySelector('#name').value;
+  var itemPrice = document.querySelector('#price').value;
+
+  if(itemName == '' || itemPrice == '' || itemName == null || itemPrice == null){
+    alert("Ops, parece que você não preencheu um campo. Tente novamente.")
+  }else{
+
+    // adiciona um novo item
+    bill.addItem(new Item(itemName, itemPrice));
+
+    //Soma os precos de itens adicionados
+    bill.billTotal();
+
+    //renderiza a comanda
+    bill.render(); 
+
+    limparInput();
+
+  }
+}
+
+function billtotal2(){
+  
+  var itemPrice = document.querySelector('#price').value;
+
+  bill.billTotal2(itemPrice);
+
+  bill.render();
+
+}
+
+function limparInput(){
+  let inputName = document.querySelector('#name');
+  let inputPrice = document.querySelector('#price');
+
+  inputName.value = '';
+  inputPrice.value = '';
+
 }
 
 function printBill() {
   window.print();
+  bill.items.length = 0;
+
+  let elementTotal = document.getElementById('total');
+  elementTotal.innerHTML = 'R$ 0,00';
+  limparInput();
+  bill.render();
+  
 }
+
